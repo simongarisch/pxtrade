56d96d84fa82c2f853f0f15adfb0ed20b98cc901
simongarisch <gatman946@gmail.com>
2020-09-08 19:13:22 +1000

    adding observer with abstract method

--- pytrade/observable.py
+++ pytrade/observable.py
@@ -1,8 +1,10 @@
 from weakref import WeakSet
+from abc import ABC, abstractmethod
 
 
 class Observable:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self._observers = WeakSet()
 
     def add_observer(self, observer):
@@ -14,3 +16,10 @@ class Observable:
     def notify_observers(self):
         for observer in self._observers:
             observer.observable_update(self)
+
+
+class Observer(ABC):
+    """ All observers must implement a observable_update method. """
+    @abstractmethod
+    def observable_update(self):
+        raise NotImplementedError()  # pragma: no cover
