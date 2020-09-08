b10217b8de5f4f6fb58929c42d632bf9f87cf3b0
simongarisch <gatman946@gmail.com>
2020-09-09 07:10:16 +1000

    extending portfolio

--- pytrade/assets/portfolio.py
+++ pytrade/assets/portfolio.py
@@ -1,37 +1,50 @@
-""" A portfolio represents a collection of assets that'll
-    be valued in some base currency.
+"""
+A portfolio represents a collection of assets that'll be valued
+in some base currency.
+Whenever an asset is transfered to / from or traded into the portfolio
+the revalue method is called.
 """
 from collections import defaultdict
 from numbers import Real
-from .asset import Asset
+from .asset import Asset, VariablePriceAsset
 from .cash import Cash
 from .codes import check_currency_code
-from .fx_rates import FxRate
+from .fx_rates import FxRate, is_equivalent_pair
 from ..observable import Observer
 from ..settings import get_default_currency_code
 
 
 class Portfolio(Observer):
     """ A portfolio will observe the assets it holds. """
-    def __init__(self, base_currency=None):
+    def __init__(self, base_currency_code=None):
         super().__init__()
-        if base_currency is None:
-            base_currency = get_default_currency_code()
-        base_currency = check_currency_code(base_currency)
-        self._base_currency = base_currency
+        if base_currency_code is None:
+            base_currency_code = get_default_currency_code()
+        base_currency_code = check_currency_code(base_currency_code)
+        self._base_currency_code = base_currency_code
         self._holdings = defaultdict(lambda: 0)
         self._value = 0
 
     @property
     def value(self):
-        """ value is read only. """
+        """ value is read only after init. """
         return self._value
+
+    @property
+    def base_currency_code(self):
+        """ base_currency_code is read only after init. """
+        return self._base_currency_code
 
     def transfer(self, asset, units):
         self._holdings[asset] += units
         self._revalue()
 
     def trade(self, asset, units, consideration=None):
+        if not isinstance(asset, Asset):
+            raise TypeError("Expecting Asset instance.")
+        if not isinstance(units, int):
+            raise TypeError("Expecting int.")
+
         if consideration is None:
             asset_local_value = asset.local_value
             if asset_local_value is None:
@@ -52,15 +65,33 @@ class Portfolio(Observer):
             cash = Cash(currency_code)
         self._holdings[asset] += units
         self._holdings[cash] += consideration
+        self._revalue()
 
-    def observable_update(self):
+    def observable_update(self, observable):
         self._revalue()
 
     def _revalue(self):
         value = 0
-        base_currency = self._base_currency
+        base_currency_code = self._base_currency_code
         for asset, units in self._holdings.items():
-            fx_pair = base_currency + asset.currency_code
+            # make sure to observe all assets held and the currencies they
+            # are quoted in.
+            fx_pair = base_currency_code + asset.currency_code
+            if not is_equivalent_pair(fx_pair):
+                fx_observable = FxRate.get_observable_instance(fx_pair)
+                fx_observable.add_observer(self)
+            if isinstance(asset, VariablePriceAsset):
+                asset.add_observer(self)
+
+            # calculate the value of this holding to the portfolio
             fx_rate = FxRate.get(fx_pair)
-            value += asset.local_value * fx_rate * units
+            value += asset.local_value / fx_rate * units
         self._value = value
+
+    def __str__(self):
+        return "Portfolio(%s): \n" % self.base_currency_code + "\n".join(
+            [
+                str(asset) + ": " + format(int(units), ",d")
+                for asset, units in self._holdings.items()
+            ]
+        )
---
+++ tests/test_portfolio.py
@@ -0,0 +1,104 @@
+import pytest
+from pytrade.assets import FxRate, Stock, Cash, Portfolio
+
+
+def test_portfolio_init():
+    portfolio = Portfolio("AUD")
+    assert portfolio.base_currency_code == "AUD"
+    assert portfolio.value == 0
+
+
+def test_base_currency():
+    with pytest.raises(TypeError):
+        Portfolio(123)
+    with pytest.raises(ValueError):
+        Portfolio("AUDX")
+    portfolio = Portfolio("AUD")
+    assert portfolio.base_currency_code == "AUD"
+    with pytest.raises(AttributeError):
+        # base currency code is read only after init
+        portfolio.base_currency_code = "USD"
+
+
+def test_transfer_basec_currency():
+    portfolio = Portfolio("AUD")
+    aud = Cash("AUD")
+    portfolio.transfer(aud, 1000)
+    assert portfolio.value == 1000
+
+
+def test_transfer_cash_multiple_currencies():
+    portfolio = Portfolio("AUD")
+    assert portfolio.value == 0
+
+    aud = Cash("AUD")
+    usd = Cash("USD")
+    audusd = FxRate("AUDUSD", 0.65)
+
+    portfolio.transfer(aud, 1000)
+    assert portfolio.value == 1000
+
+    portfolio.transfer(usd, 1000)
+    assert portfolio.value == 1000 + 1000 / audusd.rate
+
+
+def test_transfer_stock():
+    portfolio = Portfolio("AUD")
+    assert portfolio.value == 0
+    zzb = Stock("ZZB AU", 2.50, currency_code="AUD")
+    portfolio.transfer(zzb, 1000)
+    assert portfolio.value == 1000 * 2.50
+
+    aapl = Stock("AAPL US", 110, currency_code="USD")
+
+    audusd = FxRate("AUDUSD", 0.65)
+    portfolio.transfer(aapl, 1000)
+    assert portfolio.value == 1000 * 2.50 + 1000 * 110 / audusd.rate
+
+    portfolio.transfer(aapl, -1000)
+    assert portfolio.value == 1000 * 2.50  # just zzb left
+
+    portfolio.transfer(zzb, -1000)
+    assert portfolio.value == 0
+
+
+def test_cash_holdings_observed():
+    portfolio = Portfolio("AUD")
+    assert portfolio.value == 0
+
+    aud = Cash("AUD")
+    portfolio.transfer(aud, 1000)
+    assert portfolio.value == 1000
+
+    usd = Cash("USD")
+    audusd = FxRate("AUDUSD", 0.65)
+    portfolio.transfer(usd, 1000)
+    assert portfolio.value == 1000 + 1000 / audusd.rate
+    audusd.rate == 0.55
+    assert portfolio.value == 1000 + 1000 / audusd.rate
+    audusd.rate == 0.75
+    assert portfolio.value == 1000 + 1000 / audusd.rate
+
+
+def test_stock_observed():
+    portfolio = Portfolio("AUD")
+    zzb = Stock("ZZB AU", 2.50, currency_code="AUD")
+    portfolio.transfer(zzb, 1000)
+    assert portfolio.value == 1000 * 2.50
+    zzb.price = 2
+    assert portfolio.value == 1000 * 2
+
+    aapl = Stock("AAPL US", 120, currency_code="USD")
+    audusd = FxRate("AUDUSD", 0.65)
+    portfolio.transfer(aapl, 1000)
+    assert round(portfolio.value, 1) == round(
+        1000 * 2 + 1000 * 120 / audusd.rate, 1
+    )
+    aapl.price = 130
+    assert round(portfolio.value, 1) == round(
+        1000 * 2 + 1000 * 130 / audusd.rate, 1
+    )
+    audusd.rate = 0.75
+    assert round(portfolio.value, 1) == round(
+        1000 * 2 + 1000 * 130 / audusd.rate, 1
+    )
