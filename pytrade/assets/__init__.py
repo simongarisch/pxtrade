0feaeb627d84696563f4cc7c850844a953a18081
simongarisch <gatman946@gmail.com>
2020-09-09 07:09:19 +1000

    importing portfolio and fx

--- pytrade/assets/__init__.py
+++ pytrade/assets/__init__.py
@@ -5,3 +5,5 @@ from .asset import (  # noqa: F401
 )
 from .cash import Cash  # noqa: F401
 from .stock import Stock  # noqa: F401
+from .portfolio import Portfolio  # noqa: F401
+from .fx_rates import FxRate  # noqa: F401
