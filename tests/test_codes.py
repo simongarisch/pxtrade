b69892dfc71a1bb6fee91069def85cb0fa157396
simongarisch <gatman946@gmail.com>
2020-09-08 19:14:41 +1000

    adding to tests

---
+++ tests/test_codes.py
@@ -0,0 +1,54 @@
+import pytest
+from pytrade.assets.codes import (
+    check_code,
+    check_currency_code,
+    Codes,
+)
+
+
+class Test:
+    pass
+
+
+def test_check_code():
+    assert check_code("AAPL") == "AAPL"
+    assert check_code(" aapl ") == "AAPL"
+    with pytest.raises(TypeError):
+        check_code(123)
+
+
+def test_check_currency_code():
+    assert check_currency_code("USD") == "USD"
+    assert check_currency_code(" usd ") == "USD"
+    with pytest.raises(TypeError):
+        check_currency_code(123)
+    with pytest.raises(ValueError):
+        check_currency_code("XXXX")  # must be only 3 chars
+
+
+def test_codes():
+    codes = Codes()
+
+    # there are no registered codes to start with
+    assert len(codes.get_registered_codes()) == 0
+    assert codes.code_in_use("XXX") is False
+    assert codes.get_object_for_code("XXX") is None
+
+    # add one code
+    obj = Test()
+    codes.register("XXX", obj)
+    assert len(codes.get_registered_codes()) == 1
+    assert codes.code_in_use("XXX") is True
+    assert codes.get_object_for_code("XXX") is obj
+
+    # check that this must be unique
+    codes.register("XXX", obj)
+    assert len(codes.get_registered_codes()) == 1
+
+    obj2 = Test()
+    with pytest.raises(ValueError):
+        codes.register("XXX", obj2)
+
+    # the objects are weakly referenced
+    del obj
+    assert len(codes.get_registered_codes()) == 0
--- tests/test_observable.py
+++ tests/test_observable.py
@@ -21,3 +21,24 @@ def test_observable():
     observable.status = "ready"
     observable.notify_observers()
     assert observer.observes == "ready"
+
+
+def test_observer_add_discard():
+    observable = Something()
+    observer1 = Observer()
+    observer2 = Observer()
+
+    for observer in [observer1, observer2]:
+        observable.add_observer(observer)
+        assert observer.observes is None
+
+    observable.status = "round1"
+    observable.notify_observers()
+    for observer in [observer1, observer2]:
+        assert observer.observes == "round1"
+
+    observable.remove_observer(observer2)
+    observable.status = "round2"
+    observable.notify_observers()
+    assert observer1.observes == "round2"
+    assert observer2.observes == "round1"
