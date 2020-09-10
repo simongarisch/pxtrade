5f573b795ff720f4629b89e54151f5f3543403ea
simongarisch <gatman946@gmail.com>
2020-09-10 18:22:05 +1000

    modifying trade

--- pytrade/trade.py
+++ pytrade/trade.py
@@ -3,13 +3,18 @@ from .assets import Asset
 
 
 class ProposedTrade:
-    def __init__(self, asset_code, units):
-        asset = self._asset = Asset.get_asset_for_code(asset_code)
-        if asset is None:
-            raise ValueError("Asset code '%s' doesn't exist." % asset_code)
+    def __init__(self, asset, units):
+        if isinstance(asset, Asset):
+            self._asset = asset
+        else:
+            if not isinstance(asset, str):
+                raise TypeError("Expecting Asset instance or asset code.")
+            asset = self._asset = Asset.get_asset_for_code(asset)
+            if asset is None:
+                raise ValueError("Asset code '%s' doesn't exist." % asset)
         if not isinstance(units, int):
             raise TypeError("Units for proposed trade must be an integer.")
-        self._asset_code = asset_code
+        self._asset_code = asset.code
         self._units = units
 
     @property
--- tests/test_trade.py
+++ tests/test_trade.py
@@ -12,6 +12,16 @@ def test_proposed_trade_init():
     assert proposed_trade.units == 100
 
 
+def test_proposed_trade_init_with_asset():
+    zzb = Stock("ZZB AU", 2.50, currency_code="AUD")
+    proposed_trade = trade.ProposedTrade(zzb, 100)
+    assert proposed_trade.asset is zzb
+    assert proposed_trade.asset_code == "ZZB AU"
+    assert proposed_trade.units == 100
+    with pytest.raises(TypeError):
+        trade.ProposedTrade(123, 100)
+
+
 def test_proposed_trade_immutable():
     zzz = Stock("ZZZ AU", 2.50, currency_code="AUD")
     assert zzz.code == "ZZZ AU"
