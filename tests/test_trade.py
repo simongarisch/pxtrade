fa02bb5f3ad397fa0aaa25d1a4801ba93902212e
simongarisch <gatman946@gmail.com>
2020-09-12 13:59:46 +1000

    refactored test_trade

--- tests/test_trade.py
+++ tests/test_trade.py
@@ -1,58 +1,61 @@
 import pytest
-from pytrade.assets import Stock
-from pytrade import trade
+from pytrade.assets import Stock, Portfolio
+from pytrade.trade import ProposedTrade
 
 
-def test_proposed_trade_init():
-    zzb = Stock("ZZB AU", 2.50, currency_code="AUD")
-    assert isinstance(zzb, Stock)
-    assert zzb.code == "ZZB AU"
-    proposed_trade = trade.ProposedTrade("ZZB AU", 100)
-    assert proposed_trade.asset_code == "ZZB AU"
-    assert proposed_trade.units == 100
+class TestTrade(object):
+    def setup_method(self, *args):
+        portfolio = self.portfolio = Portfolio("AAA")
+        self.stock = Stock("ZZB AU", 2.50, currency_code="AUD")
+        self.proposed_trade = ProposedTrade(portfolio, "ZZB AU", 100)
 
+    def teardown_method(self, *args):
+        del self.stock
+        del self.portfolio
+        del self.proposed_trade
 
-def test_proposed_trade_init_with_asset():
-    zzb = Stock("ZZB AU", 2.50, currency_code="AUD")
-    proposed_trade = trade.ProposedTrade(zzb, 100)
-    assert proposed_trade.asset is zzb
-    assert proposed_trade.asset_code == "ZZB AU"
-    assert proposed_trade.units == 100
-    with pytest.raises(TypeError):
-        trade.ProposedTrade(123, 100)
+    def test_proposed_trade_init(self):
+        proposed_trade = self.proposed_trade
+        assert proposed_trade.portfolio is self.portfolio
+        assert proposed_trade.asset is self.stock
+        assert proposed_trade.asset_code == "ZZB AU"
+        assert proposed_trade.units == 100
 
-
-def test_proposed_trade_immutable():
-    zzz = Stock("ZZZ AU", 2.50, currency_code="AUD")
-    assert zzz.code == "ZZZ AU"
-    proposed_trade = trade.ProposedTrade("ZZZ AU", 200)
-    assert proposed_trade.asset_code == "ZZZ AU"
-    assert proposed_trade.units == 200
-    assert proposed_trade.asset is zzz
-    with pytest.raises(AttributeError):
-        proposed_trade.asset_code = "YYY AU"
-    with pytest.raises(AttributeError):
-        proposed_trade.units = 500
-
-
-def test_proposed_trade_no_asset():
-    with pytest.raises(ValueError):
-        trade.ProposedTrade(
-            "NO_ASSET_WITH_THIS_CODE", 200
+    def test_proposed_trade_init_with_asset(self):
+        proposed_trade = ProposedTrade(
+            self.portfolio, self.stock, 300,
         )
+        assert proposed_trade.portfolio is self.portfolio
+        assert proposed_trade.asset is self.stock
+        assert proposed_trade.asset_code == "ZZB AU"
+        assert proposed_trade.units == 300
+        with pytest.raises(TypeError):
+            ProposedTrade(123, 100)
 
+    def test_proposed_trade_immutable(self):
+        proposed_trade = self.proposed_trade
+        with pytest.raises(AttributeError):
+            proposed_trade.portfolio = self.portfolio
+        with pytest.raises(AttributeError):
+            proposed_trade.asset = self.stock
+        with pytest.raises(AttributeError):
+            proposed_trade.asset_code = "ZZB AU"
+        with pytest.raises(AttributeError):
+            proposed_trade.units = 500
 
-def test_units_must_be_int():
-    zzb = Stock("ZZB AU", 2.50, currency_code="AUD")
-    proposed_trade = trade.ProposedTrade("ZZB AU", 100)
-    assert proposed_trade.asset is zzb
-    assert proposed_trade.units == 100
-    with pytest.raises(TypeError):
-        proposed_trade = trade.ProposedTrade("ZZB AU", "100")
+    def test_proposed_trade_no_asset(self):
+        with pytest.raises(ValueError):
+            ProposedTrade(
+                self.portfolio, "NO_ASSET_WITH_THIS_CODE", 200
+            )
 
+    def test_units_must_be_int(self):
+        with pytest.raises(TypeError):
+            ProposedTrade(
+                self.portfolio, "ZZB AU", "100"
+            )
 
-def test_trade_str():
-    zzb = Stock("ZZB AU", 2.50, currency_code="AUD")
-    proposed_trade = trade.ProposedTrade(zzb, 100)
-    tradestr = str(proposed_trade)
-    assert tradestr == "ProposedTrade('ZZB AU', 100)"
+    def test_trade_str(self):
+        tradestr = str(self.proposed_trade)
+        print(tradestr)
+        assert tradestr == "ProposedTrade(Portfolio('AAA'), 'ZZB AU', 100)"
