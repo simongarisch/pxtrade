de81434d366fcca01e6361c259d76f3b4f8b228e
simongarisch <gatman946@gmail.com>
2020-09-12 13:58:06 +1000

    associate portfolio with proposed trade

--- pytrade/trade.py
+++ pytrade/trade.py
@@ -1,21 +1,36 @@
 """ Defines a proposed trade. """
-from .assets import Asset
+from typing import Union
+from .assets import Asset, Portfolio
+
+
+def _check_asset(asset: Union[Asset, str]):
+    """ If an asset is passed then return this asset.
+        However, if an asset code is passed make sure this is valid.
+    """
+    if isinstance(asset, Asset):
+        return asset
+    if not isinstance(asset, str):
+        raise TypeError("Expecting Asset instance or asset code.")
+    asset = Asset.get_asset_for_code(asset)
+    if asset is None:
+        raise ValueError("Asset code '%s' doesn't exist." % asset)
+    return asset
 
 
 class ProposedTrade:
-    def __init__(self, asset, units):
-        if isinstance(asset, Asset):
-            self._asset = asset
-        else:
-            if not isinstance(asset, str):
-                raise TypeError("Expecting Asset instance or asset code.")
-            asset = self._asset = Asset.get_asset_for_code(asset)
-            if asset is None:
-                raise ValueError("Asset code '%s' doesn't exist." % asset)
+    def __init__(self, portfolio, asset, units):
+        if not isinstance(portfolio, Portfolio):
+            raise TypeError("Expecting a Portfolio instance.")
         if not isinstance(units, int):
             raise TypeError("Units for proposed trade must be an integer.")
+        self._portfolio = portfolio
+        asset = self._asset = _check_asset(asset)
         self._asset_code = asset.code
         self._units = units
+
+    @property
+    def portfolio(self):
+        return self._portfolio
 
     @property
     def asset(self):
@@ -32,9 +47,9 @@ class ProposedTrade:
     def __str__(self):
         return (
             self.__class__.__name__
-            + "('"
-            + self._asset_code
-            + "', "
+            + "("
+            + "Portfolio('%s')" % self._portfolio.base_currency_code
+            + ", '" + self._asset_code + "', "
             + str(self._units)
             + ")"
         )
