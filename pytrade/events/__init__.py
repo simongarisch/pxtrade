dd8eb75031f27c9fc2dc35a04cbe77c2084cb4ae
simongarisch <gatman946@gmail.com>
2020-09-22 19:58:59 +1000

    renamed module import

--- pytrade/events/__init__.py
+++ pytrade/events/__init__.py
@@ -1,5 +1,5 @@
 from .base import AbstractEvent  # noqa: F401
 from .asset_price_event import AssetPriceEvent  # noqa: F401
 from .fx_rate_event import FxRateEvent  # noqa: F401
-from .proposed_trade_event import TradeEvent  # noqa: F401
+from .trade_event import TradeEvent  # noqa: F401
 from .indicator_event import IndicatorEvent  # noqa: F401
