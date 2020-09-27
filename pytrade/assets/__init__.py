1feca8450b07d0ec8f5deac7f5f38f889e8446e1
simongarisch <gatman946@gmail.com>
2020-09-28 07:32:44 +1000

    adding to assets imports

--- pytrade/assets/__init__.py
+++ pytrade/assets/__init__.py
@@ -3,7 +3,8 @@ from .asset import (  # noqa: F401
     StaticPriceAsset,
     VariablePriceAsset,
 )
-from .cash import Cash  # noqa: F401
+from .cash import Cash, get_cash  # noqa: F401
 from .stock import Stock  # noqa: F401
 from .portfolio import Portfolio  # noqa: F401
 from .fx_rates import FxRate  # noqa: F401
+from .codes import check_currency_code  # noqa: F401
