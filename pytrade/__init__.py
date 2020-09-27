ffc3321d049a27bf90a485fae94dc366779dffd0
simongarisch <gatman946@gmail.com>
2020-09-28 08:51:40 +1000

    adding imports for trade and broker

--- pytrade/__init__.py
+++ pytrade/__init__.py
@@ -2,3 +2,5 @@ from . import assets  # noqa: F401
 from .events.base import AbstractEvent  # noqa: F401
 from .events_queue import EventsQueue  # noqa: F401
 from .backtest import Backtest  # noqa: F401
+from .trade import Trade  # noqa: F401
+from .broker import Broker  # noqa: F401
