f6d00648b7afb94e418ac1580c6b58e024a1e4b2
simongarisch <gatman946@gmail.com>
2020-09-30 18:47:12 +1000

    adding strategy module

---
+++ pytrade/strategy.py
@@ -0,0 +1,13 @@
+"""
+Defines a strategy for out backtest.
+Each strategy will return trades to be executed.
+"""
+from abc import ABC, abstractmethod
+from typing import Union, List
+from pytrade import trade
+
+
+class Strategy(ABC):
+    @abstractmethod
+    def generate_trades(self) -> Union[None, trade.Trade, List[trade.Trade]]:
+        raise NotImplementedError
