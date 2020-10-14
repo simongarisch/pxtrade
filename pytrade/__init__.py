25ee28f13dca8c0548a68315d5a1e2526d044223
simongarisch <gatman946@gmail.com>
2020-10-14 11:11:46 +1100

    adding Strategy and History classes to import

--- pytrade/__init__.py
+++ pytrade/__init__.py
@@ -1,5 +1,7 @@
 from .events.base import AbstractEvent  # noqa: F401
 from .events_queue import EventsQueue  # noqa: F401
+from .strategy import Strategy  # noqa: F401
 from .backtest import Backtest  # noqa: F401
+from .history import History  # noqa: F401
 from .trade import Trade  # noqa: F401
 from .broker import Broker  # noqa: F401
