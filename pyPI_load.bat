787d342e3cb47914acb95313dfba381716e74b3c
simongarisch <gatman946@gmail.com>
2020-10-19 08:27:08 +1100

    adding pypi load batch file

---
+++ pyPI_load.bat
@@ -0,0 +1,13 @@
+call activate py38
+
+rem make sure we are up to date
+python -m pip install --user --upgrade setuptools wheel
+python -m pip install --upgrade twine
+
+rem now build
+python setup.py sdist bdist_wheel
+
+rem and push
+python -m twine upload dist/*
+
+pause
